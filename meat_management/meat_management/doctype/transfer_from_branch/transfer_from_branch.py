880154750d82b797b2fc48956a3d5bd868559482
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-08-18 19:48:19 +0000

    feat: transfer fro branch

--- meat_management/meat_management/doctype/transfer_from_branch/transfer_from_branch.py
+++ meat_management/meat_management/doctype/transfer_from_branch/transfer_from_branch.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2024, mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class TransferFromBranch(Document):
-	pass
+	def validate(self):
+		self.send_data()
+
+	def send_data(self):
+		# if self.docstatus == 1:
+			items = []
+			for item in self.item:
+				items.append({
+					'item_code':item.item,
+					'qty':item.qty
+				})
+			transfer_item = frappe.get_doc({
+				'doctype':"Stock Entry",
+				'stock_entry_type':"Material Receipt",
+				'to_warehouse': self.warehouse,
+				'items':items
+			})
+			transfer_item.insert(ignore_permissions=True)
+			transfer_item.save()
+			transfer_item.submit()
+
