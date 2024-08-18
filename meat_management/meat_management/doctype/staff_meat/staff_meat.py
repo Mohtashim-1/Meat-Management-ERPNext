bd900f64c41dc495e54a9a9f54081fc6c09468c6
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-08-18 23:10:55 +0000

    chore: condition for posting date added

--- meat_management/meat_management/doctype/staff_meat/staff_meat.py
+++ meat_management/meat_management/doctype/staff_meat/staff_meat.py
@@ -3,12 +3,14 @@
 
 import frappe
 from frappe.model.document import Document
+import frappe.utils
 
 
 class StaffMeat(Document):
 	def validate(self):
 		self.data_to_bin()
 		self.data_to_se()
+		self.check_period()
 
 	@frappe.whitelist()
 	def get_data(self):
@@ -67,6 +69,32 @@ class StaffMeat(Document):
 				se.submit()
 			else:
 				frappe.throw("No valid items to process.")
+	
+	def check_period(self):
+		try:
+			doc = frappe.get_doc("Staff Meat", self.name)
+			meat_period = frappe.get_doc("Meat Period", doc.period)
+			meat_period_start = meat_period.from_date
+			if meat_period_start:
+				posting_date = self.posting_date
+				if isinstance(posting_date, str):
+					posting_date = frappe.utils.getdate(posting_date)
+				
+				if posting_date < meat_period_start:
+					frappe.throw(f"Posting date {posting_date} cannot be earlier than the period start date {meat_period_start}")
 
+			meat_period_end = meat_period.to_date
+			if meat_period_end:
+				posting_date = self.posting_date
+				if isinstance(posting_date, str):
+					posting_date = frappe.utils.getdate(posting_date)
+				
+				if posting_date > meat_period_end:
+					frappe.throw(f"Posting date {posting_date} cannot be greater than the period End date {meat_period_start}")
 
-			
+				
+				
+		except frappe.DoesNotExistError as e:
+			frappe.msgprint(f"Document not found: {e}")
+		except Exception as e:
+			frappe.msgprint(f"Error occurred: {e}")
