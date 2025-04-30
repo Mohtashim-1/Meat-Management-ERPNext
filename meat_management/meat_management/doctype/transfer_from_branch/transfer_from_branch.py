e4c4e1edd85c210b56dd48590d7b6ced9a4aed3b
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-05-01 01:56:48 +0500

    chore: fix in transfer from branch

--- meat_management/meat_management/doctype/transfer_from_branch/transfer_from_branch.py
+++ meat_management/meat_management/doctype/transfer_from_branch/transfer_from_branch.py
@@ -6,24 +6,47 @@ from frappe.model.document import Document
 
 
 class TransferFromBranch(Document):
-	def validate(self):
-		self.send_data()
+    def validate(self):
+        self.send_data()
+        self.transfer_branch_to_slaughter()
 
-	def send_data(self):
-		if self.docstatus == 1:
-			items = []
-			for item in self.item:
-				items.append({
-					'item_code':item.item,
-					'qty':item.qty
-				})
-			transfer_item = frappe.get_doc({
-				'doctype':"Stock Entry",
-				'stock_entry_type':"Material Receipt",
-				'to_warehouse': self.warehouse,
-				'items':items
-			})
-			transfer_item.insert(ignore_permissions=True)
-			transfer_item.save()
-			transfer_item.submit()
+    def send_data(self):
+        bn = frappe.get_doc("Branch Name", self.branch)
+        br_w = bn.warehouse
+        if self.docstatus == 1:
+            items = []
+            for item in self.item:
+                items.append({
+                    'item_code': item.item,
+                    'qty': item.qty
+                })
+            transfer_item = frappe.get_doc({
+                'doctype': "Stock Entry",
+                'stock_entry_type': "Material Receipt",
+                'to_warehouse': br_w,
+                'items': items
+            })
+            transfer_item.insert(ignore_permissions=True)
+            transfer_item.submit()
 
+    def transfer_branch_to_slaughter(self):
+        bn = frappe.get_doc("Branch Name", self.branch)
+        br_w = bn.warehouse
+        sl = frappe.get_doc("Slaughter", self.slaughter)
+        sl_w = sl.warehouse
+        if self.docstatus == 1:
+            items = []
+            for item in self.item:
+                items.append({
+                    'item_code': item.item,
+                    'qty': item.qty
+                })
+            transfer_item = frappe.get_doc({
+                'doctype': "Stock Entry",
+                'stock_entry_type': "Material Transfer",
+                'from_warehouse': br_w,
+                'to_warehouse': sl_w,
+                'items': items
+            })
+            transfer_item.insert(ignore_permissions=True)
+            transfer_item.submit()
