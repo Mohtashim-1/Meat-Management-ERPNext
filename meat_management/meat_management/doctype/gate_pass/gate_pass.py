e02db676d7d6458b140fc56deb7117e670deb688
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-05-01 03:08:17 +0500

    chore: gatepass

--- meat_management/meat_management/doctype/gate_pass/gate_pass.py
+++ meat_management/meat_management/doctype/gate_pass/gate_pass.py
@@ -1,9 +1,31 @@
 # Copyright (c) 2025, mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class GatePass(Document):
-	pass
+    def validate(self):
+        self.send_data()
+
+    def send_data(self):
+        bn = frappe.get_doc("Slaughter", self.slaughter)
+        br_w = bn.warehouse
+        if self.docstatus == 1:
+            items = []
+            for item in self.gate_pass_item:
+                items.append({
+                    'item_code': item.item,
+                    'qty': item.qty,
+                    'uom': item.uom,
+                    's_warehouse': br_w
+                })
+            transfer_item = frappe.get_doc({
+                'doctype': "Stock Entry",
+                'stock_entry_type': "Material Issue",
+                's_warehouse': br_w,
+                'items': items
+            })
+            transfer_item.insert(ignore_permissions=True)
+            transfer_item.submit()
